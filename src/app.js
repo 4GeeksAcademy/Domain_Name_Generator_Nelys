3f6e574d85eb9190ee1498494c034b2acf4f8dc7
NelysT <139475672+NelysT@users.noreply.github.com>
2023-08-16 19:20:13 +0000

    funcion creada, aparece en consola

--- src/app.js
+++ src/app.js
@@ -8,4 +8,31 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".es"];
+
+  let miDominio = "";
+
+  for (let pronounIndex = 0; pronounIndex < pronoun.length; pronounIndex++) {
+    for (let adjIndex = 0; adjIndex < adj.length; adjIndex++) {
+      for (let nounIndex = 0; nounIndex < noun.length; nounIndex++) {
+        for (
+          let extensionIndex = 0;
+          extensionIndex < extension.length;
+          extensionIndex++
+        ) {
+          miDominio +=
+            pronoun[pronounIndex] +
+            adj[adjIndex] +
+            noun[nounIndex] +
+            extension[extensionIndex] +
+            "\n";
+        }
+      }
+    }
+  }
+  console.log(miDominio);
 };
